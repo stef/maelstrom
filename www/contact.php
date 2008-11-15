d7d0b71ef01336a1c3c0fe3642029cfd434c932f
Stefan Marsiske <stefan.marsiske@gmail.com>
2008-11-15 02:37:27 +0100

    [enh] added contact info [enh] stylesheet

---
+++ www/contact.php
@@ -0,0 +1,41 @@
+<?php
+include_once("mailyze.php");
+
+if(isset($_GET['c'])) {
+   $person=$_GET['c'];
+} else {
+   $person=$MAILBOXOWNER;
+}
+?>
+   <html>
+   <head>
+      <script type="text/javascript" src="maelstrom.js"></script>
+      <script src="jquery.js" type="text/javascript" charset="utf-8"></script>
+      <script src="jquery.sparkline.js" type="text/javascript" charset="utf-8"></script>
+      <script type="text/javascript" src="tagcloud.js"></script>
+      <script type="text/javascript">
+         $(document).ready(function() {
+            var params="&c=<?php print urlencode($person)?>";
+            var query="mailyze.php?op=contactMails"+params;
+            $.getJSON(query,loadSparkline);
+            var query="mailyze.php?op=secondContacts"+params;
+            $.getJSON(query,drawTagcloud);
+         });
+      </script>
+      <link href="style.css" rel="stylesheet" type="text/css" />
+   </head>
+   <body>
+      <!-- mikor leveleztem vele/mennyit sparkline over all time
+      kik a kozos kontaktok? tagcloud
+      subjects - simile -->
+
+      <h1 id="pagetitle"><?print $person?></h1>
+      <div id="sparkline" > </div>
+      <div style="width: 480px; font-size: 12px; margin: 0px 10px 30px;">
+         <span style="float: left;" id="startdate" ></span>
+         <span style="float: right;" id="enddate" ></span>
+      </div>
+      <div id="tagcloud"></div>
+      <a href="timecloud.html">back to timecloud</a>
+   </body>
+</html>
--- www/mailyze.php
+++ www/mailyze.php
@@ -3,6 +3,86 @@
 $MAILBOXOWNER="Marsiske Stefan";
 $dburl='sqlite:'.$_SERVER['DOCUMENT_ROOT'].'/mailyze/db/messages.db';
 
+// get all mails from person
+
+function contactMails($db) {
+   global $MAILBOXOWNER;
+   list($start, $time)=mailTimeFrame($db);
+
+   if(isset($_GET['start'])) {
+      $start=$_GET['start'];
+   }
+   if(isset($_GET['end'])) {
+      $end=$_GET['end'];
+   }
+   if(isset($_GET['c'])) {
+      $person=$_GET['c'];
+   } else {
+      $person=$MAILBOXOWNER;
+   }
+
+   $q="select count(message.id) as count,
+              date(message.delivered) as delivered
+         from message, role, email, person 
+         where message.id=role.msg_id and 
+               role.email_id=email.id and
+               email.owner_id==person.id and
+               person.fullname=='$person'
+         group by delivered
+         order by delivered"; //and
+               //date(message.delivered)>='$start' AND
+               //date(message.delivered)<'$end'";
+   // TODO add window handling if needed
+
+   foreach ($db->query($q) as $row) {
+      $results[]=array('delivered' => $row['delivered'],
+                       'count' => $row['count']);
+   }
+   return ($results);
+}
+
+// get all contacts with weights for person
+
+function secondContacts($db) {
+   list($start, $time)=mailTimeFrame($db);
+
+   if(isset($_GET['start'])) {
+      $start=$_GET['start'];
+   }
+   if(isset($_GET['end'])) {
+      $end=$_GET['end'];
+   }
+   if(isset($_GET['c'])) {
+      $person=$_GET['c'];
+   } else {
+      $person=$MAILBOXOWNER;
+   }
+
+   // TODO add window handling if needed, should be enough if the subquery 
+   // return messages in the interval
+   //and date(message.delivered)>='$start' AND
+   //date(message.delivered)<'$end'";
+   $q="select person.fullname as contact,
+              count(person.id) as count,
+              max(date(message.delivered)) as lastseen
+         from message, role, email, person
+         where message.id in (select message.id 
+                             from message, role, email, person 
+                             where message.id==role.msg_id and 
+                                   role.email_id==email.id and
+                                   email.owner_id==person.id and
+                                   person.fullname like '$person') and
+              message.id==role.msg_id and
+              role.email_id==email.id and
+              email.owner_id==person.id and 
+              person.fullname not like '$person'
+         group by contact;";
+   foreach ($db->query($q) as $row) {
+      $results[]=array($row['contact'],$row['count'],$row['lastseen']);
+   }
+   return ($results);
+}
+
 function mailTimeFrame($db) {
    $q='SELECT max(delivered) AS max, min(delivered) as min FROM message';
    $result=$db->query($q)->fetch();
@@ -107,6 +187,14 @@ if(isset($_GET['op'])) {
    elseif($_GET['op']=="mailFrequency") {
       //header("Content-type: text/plain");
       print json_encode(mailFrequency($db));
+   }
+   elseif($_GET['op']=="contactMails") {
+      //header("Content-type: text/plain");
+      print json_encode(contactMails($db));
+   }
+   elseif($_GET['op']=="secondContacts") {
+      //header("Content-type: text/plain");
+      print json_encode(secondContacts($db));
    }
 }
 ?>
